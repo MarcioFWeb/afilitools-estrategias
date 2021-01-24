6381d31987f7d27f26278543040b71e0a678a6fd
Márcio Figueiredo <maofigueiredo@gmail.com>
2021-01-24 10:16:52 -0300

    Implementação "GUIDES LIST" usando PARSE SDK
    - Implementação BÁSICA do "getall" das GUIDES para validação.
    - resultado apresentado no LOG somente a princípio.

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import Parse from "parse/react-native.js";
 import AsyncStorage from '@react-native-community/async-storage';
 import keys from './constants/Keys';
+import guides from './Guide';
 
-//Before using the SDK...
 Parse.setAsyncStorage(AsyncStorage);
 Parse.initialize(keys.applicationId, keys.javascriptKey);
 Parse.serverURL = keys.serverURL;
@@ -23,9 +23,21 @@ const App = () => {
     createInstallation();
   }, []);
 
+  const Guide = Parse.Object.extend('Guide');
+  const query = new Parse.Query(Guide);
+  query.find().then((results) => {
+    // You can use the "get" method to get the value of an attribute
+    // Ex: response.get("<ATTRIBUTE_NAME>")
+    if (typeof document !== 'undefined') document.write(`Guide found: ${JSON.stringify(results)}`);
+    console.log('Guide found', results);
+  }, (error) => {
+    if (typeof document !== 'undefined') document.write(`Error while fetching Guide: ${JSON.stringify(error)}`);
+    console.error('Error while fetching Guide', error);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! TESTE COM PARSE</Text>
+      <Text>Todos os GUIAS sendo obtidos (no console)</Text>
       <StatusBar style="auto" />
     </View>
   )
