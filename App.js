fa5bc43266553813f5fbe92abc3ee59173542a80
Márcio Figueiredo <maofigueiredo@gmail.com>
2021-01-24 09:52:40 -0300

    Carga (básica) do PARSE SDK
    - Utilizando o PARSE SDK
    - Na inicialização, usando HOOK para registrar dispositivo de instalação.

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Parse from "parse/react-native.js";
+import AsyncStorage from '@react-native-community/async-storage';
+import keys from './constants/Keys';
 
-export default function App() {
+//Before using the SDK...
+Parse.setAsyncStorage(AsyncStorage);
+Parse.initialize(keys.applicationId, keys.javascriptKey);
+Parse.serverURL = keys.serverURL;
+
+const App = () => {
+  useEffect(() => {
+    createInstallation = async () => {
+    const Installation = Parse.Object.extend(Parse.Installation);
+    const installation = new Installation();
+        
+    installation.set('deviceType', Platform.OS);
+    await installation.save();
+    };
+    
+    createInstallation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! TESTE COM PARSE</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
+
 }
+ 
+export default App;
 
 const styles = StyleSheet.create({
   container: {
